3a8dd768cb54207f760e327425062792bb6fe874
Surbhi Kapoor <49453939+surbhiikapoor@users.noreply.github.com>
2024-08-10 21:07:59 -0700

    ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder, label_binarize
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
